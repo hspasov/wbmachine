b63e257a36c711dff50ca7188baf541a33abbe1a
Hristo Spasov <hristo.b.spasov@gmail.com>
2021-06-06 17:01:24 +0300

    wbminit005 Add install script

---
+++ setup/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt -y install apache2 libapache2-mod-php php php-mysql mysql-server
+mysql < install/db_create.sql
+mkdir -v /var/lib/wbmachine
+mkdir -v /usr/share/wbmachine
+cp -rv . /usr/share/wbmachine
+mkdir -pv /usr/share/wbmachine/views/public
+ln -sv /usr/share/wbmachine/config/wbmachine-process-pending-archives.service /etc/systemd/system/wbmachine-process-pending-archives.service
+ln -sv /usr/share/wbmachine/config/wbmachine-process-pending-archives.timer /etc/systemd/system/wbmachine-process-pending-archives.timer
+systemctl enable wbmachine-process-pending-archives.timer
+systemctl start wbmachine-process-pending-archives.timer
+ln -sv /usr/share/wbmachine/config/wbmachine.conf /etc/apache2/sites-available/wbmachine.conf
+a2ensite wbmachine
+a2enmod php7.4
+systemctl restart apache2
---
+++ setup/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+a2dissite wbmachine
+rm -v /etc/apache2/sites-available/wbmachine.conf
+systemctl stop wbmachine-process-pending-archives.timer
+systemctl disable wbmachine-process-pending-archives.timer
+rm -v /etc/systemd/system/wbmachine-process-pending-archives.service
+systemctl restart apache2
+mysql < install/db_drop.sql
+rm -rv /usr/share/wbmachine
+rm -rv /var/lib/wbmachine
